98089906d0593b3b6cc19178eb81da632e661a16
mmerezhko <mmerezhko@rendmate.com>
2019-06-17 16:15:40 +0300

    Created views for posts

---
+++ server/controllers/posts.js
@@ -0,0 +1,13 @@
+const Post = require("../models/post");
+
+module.exports = {
+  async getPosts(req, res, next) {
+    let posts = await Post.find({});
+
+    res.render("posts/index", { posts });
+  },
+
+  newPost(req, res, next) {
+    res.render("posts/new");
+  }
+};
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,32 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
 
-router.get('/', (req, res, next) => {
-  res.send('Shop');
+const router = express.Router();
+
+const { errorHandler } = require("../middleware");
+const { getPosts, newPost } = require("../controllers/posts");
+
+router.get("/", errorHandler(getPosts));
+
+router.get("/new", newPost);
+
+router.get("/:revid", (req, res, next) => {
+  res.send("Show");
 });
 
-router.get('/new', (req, res, next) => {
-  res.send('New');
+router.get("/:revid/edit", (req, res, next) => {
+  res.send("Edit");
 });
 
-router.get('/:revid', (req, res, next) => {
-  res.send('Show');
+router.post("/", (req, res, next) => {
+  res.send("Create");
 });
 
-router.get('/:revid/edit', (req, res, next) => {
-  res.send('Edit');
+router.put("/:revid", (req, res, next) => {
+  res.send("Update");
 });
 
-router.post('/', (req, res, next) => {
-  res.send('Create');
-});
-
-router.put('/:revid', (req, res, next) => {
-  res.send('Update');
-});
-
-router.delete('/:revid', (req, res, next) => {
-  res.send('Delete');
+router.delete("/:revid", (req, res, next) => {
+  res.send("Delete");
 });
 
 module.exports = router;
