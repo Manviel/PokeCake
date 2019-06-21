5cd0c1a142ac77ff5d57dcf2216946c68955901e
mmerezhko <mmerezhko@rendmate.com>
2019-06-21 11:10:11 +0300

    Cleanup ejs

--- server/app.js
+++ server/app.js
@@ -1,79 +1,40 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const passport = require("passport");
-const session = require("express-session");
+const { ApolloServer } = require("apollo-server");
+
 const mongoose = require("mongoose");
-const methodOverride = require("method-override");
-const engine = require("ejs-mate");
-
-const indexRouter = require("./routes/index");
-const postsRouter = require("./routes/posts");
-const reviewsRouter = require("./routes/reviews");
-
-const User = require("./models/user");
 
 const keys = require("./keys");
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
 
-const app = express();
-
-const db = mongoose.connection;
-
-mongoose.connect(keys.mongoUri, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
-
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", () => console.log("MongoDB connected"));
-
-app.engine("ejs", engine);
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(methodOverride("_method"));
-
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true
+mongoose
+  .connect(keys.mongoUri, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
   })
-);
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(err));
 
-app.use(passport.initialize());
-app.use(passport.session());
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => {
+    let authToken = null;
+    let currentUser = null;
 
-passport.use(User.createStrategy());
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        currentUser = await findOrCreateUser(authToken);
+      }
+    } catch (err) {
+      console.error(`Unable to authenticate user with token ${authToken}`);
+    }
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use("/", indexRouter);
-app.use("/posts", postsRouter);
-app.use("/posts/:id/reviews", reviewsRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => next(createError(404)));
-
-// error handler
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    return { currentUser };
+  }
 });
 
-module.exports = app;
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose");
 
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({
-  email: String,
-  image: String
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String
 });
-
-UserSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", UserSchema);
